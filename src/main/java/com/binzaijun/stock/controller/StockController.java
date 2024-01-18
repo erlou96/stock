b53c35759fe6eee7d1cb43ec08dfe7c4b9c78777
10583 <erlou96@163.com>
2024-01-18 09:18:02 +0800

    first commit

--- src/main/java/com/binzaijun/stock/controller/StockController.java
+++ src/main/java/com/binzaijun/stock/controller/StockController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/api/stock")
+@RequestMapping(value = "/stock")
 public class StockController {
 
     @Autowired
