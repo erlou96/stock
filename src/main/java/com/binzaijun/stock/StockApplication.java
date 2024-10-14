19fe0d54672d620c2f975f1f7bc5b24ffdb2a3e6
二楼 <erlou96@163.com>
2024-10-14 15:20:14 +0800

    test

--- src/main/java/com/binzaijun/stock/StockApplication.java
+++ src/main/java/com/binzaijun/stock/StockApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class StockApplication {
-
+// 程序启动入口
     public static void main(String[] args) {
         SpringApplication.run(StockApplication.class, args);
     }
