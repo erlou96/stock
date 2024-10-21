26f1f66474800c7d8eaa093979b97863ec9b2cb4
10583 <erlou96@163.com>
2024-10-21 14:50:42 +0800

    1

--- start.sh
+++ start.sh
@@ -20,3 +20,10 @@ mvn clean package -Dmaven.test.skip=true
 # 启动jar包
 nohup java -jar target/stock-1.0.jar &
 
+new_pid=`ps -ef|grep stock |grep -v grep  |awk '{print $2}'`
+
+if [ -n "$new_pid" ]; then
+  echo "Process started successfully with PID: $new_pid"
+else
+  echo "Failed to start the process."
+fi
